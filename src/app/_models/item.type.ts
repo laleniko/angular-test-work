66db050f3f990005694d14330c61286b689a089c
Nikolay <laleniko@gmail.com>
2020-02-09 17:08:06 +0300

    project init

---
+++ src/app/_models/item.type.ts
@@ -0,0 +1,5 @@
+export class ItemModel {
+    id: number;
+    name: string;
+    type: string;
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { ResultComponent } from './result/result.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FilterComponent,
+    ResultComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
